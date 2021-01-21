e33acb0715f270a0c97ef8ffdfb86ac79f142ee4
mirzaditaardani <mirza@doku.com>
2021-01-21 14:45:16 +0700

    DE-176 : OCO Magento Export Issue

--- Core/Plugin/PaymentTypeColumn.php
+++ Core/Plugin/PaymentTypeColumn.php
@@ -10,20 +10,24 @@ namespace Doku\Core\Plugin;
 
 use Magento\Framework\Message\ManagerInterface as MessageManager;
 use Magento\Sales\Model\ResourceModel\Order\Grid\Collection as SalesOrderGridCollection;
+use Magento\Framework\Registry;
 
 
 class PaymentTypeColumn
 {
     private $messageManager;
     private $collection;
+    private $registry;
 
     public function __construct(MessageManager $messageManager,
-                                SalesOrderGridCollection $collection
+                                SalesOrderGridCollection $collection,
+                                Registry $registry
     )
     {
 
         $this->messageManager = $messageManager;
         $this->collection = $collection;
+        $this->registry = $registry;
     }
 
     public function aroundGetReport(
@@ -33,19 +37,22 @@ class PaymentTypeColumn
     )
     {
         $result = $proceed($requestName);
-
         if ($requestName == 'sales_order_grid_data_source') {
             if ($result instanceof $this->collection
             ) {
-                $select = $this->collection->getSelect();
-                $select->joinLeft(
-                    ["dokutrans" => $this->collection->getTable("doku_transaction")],
-                    'main_table.entity_id = dokutrans.order_id',
-                    array('payment_type')
-                );
-                return $this->collection;
+                if (is_null($this->registry->registry('doku_transaction'))) {
+                    $select = $this->collection->getSelect();
+                    $select->joinLeft(
+
+                        ["dokutrans" => $this->collection->getTable("doku_transaction")],
+                        'main_table.entity_id = dokutrans.order_id',
+                        array('payment_type')
+                    );
+
+                    $this->registry->register('doku_transaction', true);
+                }
             }
         }
-        return $result;
+        return $this->collection;
     }
 }
